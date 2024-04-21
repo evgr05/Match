7435475b5846a422b0e3f37ad12a313abda41515
admin <admin@COMP4>
2024-04-21 21:09:34 +0300

    Добавьте файлы проекта.

---
+++ Match/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match
+{
+    public class Circle
+    {
+        public static double CalculateArea(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Радиус не может быть отрицательным.");
+            }
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус круга: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Circle.CalculateArea(radius);
+
+            Console.WriteLine($"Площадь круга с радиусом {radius} равна {area}");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+using Match;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AreaWithRadius1()
+        {
+            Assert.AreEqual(3.14159, Circle.CalculateArea(1), 0.00001);
+        }
+        [TestMethod]
+        public void AreaWithRadius0()
+        {
+            Assert.AreEqual(0, Circle.CalculateArea(0));
+        }
+        [TestMethod]
+        public void AreaWithNegativeRadius()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => Circle.CalculateArea(-1));
+        }
+    }
+}
